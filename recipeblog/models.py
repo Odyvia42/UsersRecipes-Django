61102e26aa3c3b1e1fd2272b9776f01a52719a0a
Odyvia42 <oneviajdm@gmail.com>
2022-12-05 12:56:58 +0700

    Реализована модель класса Пользователь.

--- recipeblog/models.py
+++ recipeblog/models.py
@@ -4,4 +4,15 @@ from django.db import models
 
 
 class User(models.Model):
+    ACTIVE = 'AC'
+    BLOCKED = 'BL'
+    status_choices = [
+        (ACTIVE, 'Active'),
+        (BLOCKED, 'Blocked')
+    ]
+    username = models.CharField(unique=True, max_length=25)
+    status = models.CharField(max_length=2, choices=status_choices, default=ACTIVE)
+    faves = models.TextField(default='')
+    registration_date = models.DateTimeField(auto_now_add=True, editable=False)
+    update_date = models.DateTimeField(auto_now=True)
 
