65ce0616d9febc5c717dcfe3de2c1684ff604d53
Odyvia42 <oneviajdm@gmail.com>
2022-12-06 08:42:36 +0700

    Расширено отображение списка пользователей в Django Admin - добавлены поля "статус" и "дата регистрации"

--- recipeblog/admin.py
+++ recipeblog/admin.py
@@ -4,5 +4,9 @@ from django.contrib import admin
 
 from .models import User, Recipe
 
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(Recipe)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'registration_date', 'status']
