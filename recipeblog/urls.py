8451c73724fc0a205fb8f217c8b63a003ef8aa4a
Odyvia42 <oneviajdm@gmail.com>
2022-12-12 11:29:40 +0700

    Добавлены представление и шаблон страницы для отображения списка пользователей.

--- recipeblog/urls.py
+++ recipeblog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='home'),
+    path('user-list/', views.UserListView.as_view()),
 ]
--- recipeblog/views.py
+++ recipeblog/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.views.generic import ListView
+
+from recipeblog.models import User
+
 
 # Create your views here.
 
 def index(request: HttpRequest):
     return render(request, 'index.html')
+
+
+class UserListView(ListView):
+    model = User
+    context_object_name = 'user_list'
+    template_name = 'user-list.html'
+
+
+
 
