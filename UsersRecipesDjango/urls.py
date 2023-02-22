ab7af28508c8e6205bdf7633316fe2829db58b53
Odyvia42 <oneviajdm@gmail.com>
2023-02-22 09:40:27 +0700

    Реализована навигация с главной страницы на страницы со списками пользователей и рецептов.

--- UsersRecipesDjango/urls.py
+++ UsersRecipesDjango/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from recipeblog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipeblog.urls'))
+    path('', include('recipeblog.urls')),
 ]
--- recipeblog/urls.py
+++ recipeblog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='home'),
-    path('user-list/', views.UserListView.as_view()),
-    path('recipe-list/', views.RecipeListView.as_view()),
+    path('user-list/', views.UserListView.as_view(), name='user-list'),
+    path('recipe_list/', views.RecipeListView.as_view(), name='recipe-list'),
 ]
