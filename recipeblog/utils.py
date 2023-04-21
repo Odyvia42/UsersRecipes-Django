be4a16541ff3f8db276732598b519047d3c5db50
Odyvia42 <oneviajdm@gmail.com>
2023-04-21 18:07:49 +0700

    Обработка лайков и добавление/удаление избранного в списке рецептов вынесены в отдельную функцию.

--- recipeblog/utils.py
+++ recipeblog/utils.py
@@ -8,3 +8,15 @@ def get_paginated(request, queryset):
     p = Paginator(queryset, 5)
     page = request.GET.get('page')
     return p.get_page(page)
+
+def check_likes_faves(request, queryset):
+    for recipe in queryset:
+        if recipe.likes.filter(id=request.user.id).exists():
+            recipe.is_liked = True
+        else:
+            recipe.is_liked = False
+        if recipe.favs.filter(id=request.user.id).exists():
+            recipe.is_faved = True
+        else:
+            recipe.is_faved = False
+    return queryset
