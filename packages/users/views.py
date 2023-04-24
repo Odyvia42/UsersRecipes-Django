ff0867c57dd699a3b053ac94a1dff59339e43583
Odyvia42 <oneviajdm@gmail.com>
2023-04-24 08:41:11 +0700

    Сортировка списка всех пользователей по дате регистрации, алфавиту, количеству рецептов и рейтингу переписана с использованием отдельной служебной функции.

--- packages/users/views.py
+++ packages/users/views.py
@@ -10,7 +10,7 @@ def get_all_users():
     return User.objects.annotate(num_recipes=Count('recipe', distinct=True)).annotate(likes_amount=Count('recipe__likes'))
 
 def sort_user_list_by_reg_date_asc(request):
-    users = User.objects.annotate(num_recipes=Count('recipe', distinct=True)).annotate(likes_amount=Count('recipe__likes')).order_by(F('registration_date').asc())
+    users = get_all_users().order_by(F('registration_date').asc())
     p = Paginator(users, 5)
     page = request.GET.get('page')
     paged_users = p.get_page(page)
@@ -20,7 +20,7 @@ def sort_user_list_by_reg_date_asc(request):
                    })
 
 def sort_user_list_by_reg_date_desc(request):
-    users = User.objects.annotate(num_recipes=Count('recipe', distinct=True)).annotate(likes_amount=Count('recipe__likes')).order_by(F('registration_date').desc())
+    users = get_all_users().order_by(F('registration_date').desc())
     p = Paginator(users, 5)
     page = request.GET.get('page')
     paged_users = p.get_page(page)
@@ -30,7 +30,7 @@ def sort_user_list_by_reg_date_desc(request):
                    })
 
 def sort_user_list_by_username_asc(request):
-    users = User.objects.annotate(num_recipes=Count('recipe', distinct=True)).annotate(likes_amount=Count('recipe__likes')).order_by(F('username').asc())
+    users = get_all_users().order_by(F('username').asc())
     p = Paginator(users, 5)
     page = request.GET.get('page')
     paged_users = p.get_page(page)
@@ -40,7 +40,7 @@ def sort_user_list_by_username_asc(request):
                    })
 
 def sort_user_list_by_username_desc(request):
-    users = User.objects.annotate(num_recipes=Count('recipe', distinct=True)).annotate(likes_amount=Count('recipe__likes')).order_by(F('username').desc())
+    users = get_all_users().order_by(F('username').desc())
     users_with_recipes = User.objects.annotate(num_recipes=Count('recipe'))
     p = Paginator(users, 5)
     page = request.GET.get('page')
@@ -51,7 +51,7 @@ def sort_user_list_by_username_desc(request):
                    })
 
 def sort_user_list_by_recipes_amount_asc(request):
-    users = User.objects.annotate(num_recipes=Count('recipe', distinct=True)).annotate(likes_amount=Count('recipe__likes')).order_by(F('num_recipes').asc())
+    users = get_all_users().order_by(F('num_recipes').asc())
     p = Paginator(users, 5)
     page = request.GET.get('page')
     paged_users = p.get_page(page)
@@ -61,7 +61,7 @@ def sort_user_list_by_recipes_amount_asc(request):
                    })
 
 def sort_user_list_by_recipes_amount_desc(request):
-    users = User.objects.annotate(num_recipes=Count('recipe', distinct=True)).annotate(likes_amount=Count('recipe__likes')).order_by(F('num_recipes').desc())
+    users = get_all_users().order_by(F('num_recipes').desc())
     p = Paginator(users, 5)
     page = request.GET.get('page')
     paged_users = p.get_page(page)
@@ -71,7 +71,7 @@ def sort_user_list_by_recipes_amount_desc(request):
                    })
 
 def sort_user_list_by_rating_asc(request):
-    users = User.objects.annotate(num_recipes=Count('recipe', distinct=True)).annotate(likes_amount=Count('recipe__likes')).order_by(F('likes_amount').asc())
+    users = get_all_users().order_by(F('likes_amount').asc())
     p = Paginator(users, 5)
     page = request.GET.get('page')
     paged_users = p.get_page(page)
@@ -81,7 +81,7 @@ def sort_user_list_by_rating_asc(request):
                    })
 
 def sort_user_list_by_rating_desc(request):
-    users = User.objects.annotate(num_recipes=Count('recipe', distinct=True)).annotate(likes_amount=Count('recipe__likes')).order_by(F('likes_amount').desc())
+    users = get_all_users().order_by(F('likes_amount').desc())
     p = Paginator(users, 5)
     page = request.GET.get('page')
     paged_users = p.get_page(page)
