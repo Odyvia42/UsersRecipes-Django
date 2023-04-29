221404b00367fedb1b86b4a3c636cc8105a3ac25
Odyvia42 <oneviajdm@gmail.com>
2023-04-29 09:44:15 +0700

    Добавлены отступы между функциями

--- recipeblog/admin.py
+++ recipeblog/admin.py
@@ -9,11 +9,13 @@ from .models import User, Recipe
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'registration_date', 'status']
 
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ['title', 'dish_type', 'author', 'publication_date', 'tag_list']
 
     def get_queryset(self, request):
         return super().get_queryset(request).prefetch_related('tags')
+
     def tag_list(self, obj):
         return u", ".join(o.name for o in obj.tags.all())
