c8ab2bb7dabb13118b6a03aaee2a680693f44bdf
Odyvia42 <oneviajdm@gmail.com>
2023-05-07 08:41:41 +0700

    Добавлена возможность просмотра документации в интерфейсе django admin.

--- UsersRecipesDjango/urls.py
+++ UsersRecipesDjango/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from recipeblog import views
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('', include('recipeblog.urls')),
 ]
